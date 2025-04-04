68b457612279d4b98bbc720821f25315aa61d94e
Mirabel <mirabelnaren@hotmail.com>
2025-04-04 16:22:44 -0300

    Actualizar configuración de archivos estáticos: corregir STATIC_URL, definir STATICFILES_DIRS y ajustar STATIC_ROOT.

--- src/PaginaDeCine/settings.py
+++ src/PaginaDeCine/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,14 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 try:
     # Importar la configuracion de desarrollo cuando el archivo este disponible
