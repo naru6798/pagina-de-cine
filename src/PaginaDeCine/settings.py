91c644ca6e90938dffbc9a56a91a7c7c2704577c
Mirabel <mirabelnaren@hotmail.com>
2025-04-04 17:04:55 -0300

    Actualizar STATIC_ROOT en settings.py para apuntar a 'static'

--- src/PaginaDeCine/settings.py
+++ src/PaginaDeCine/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 try:
     # Importar la configuracion de desarrollo cuando el archivo este disponible
